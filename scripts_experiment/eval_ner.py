0c8ae2a2719de16030821b65c979c43d9678ee28
sntcristian <snt.cristian@gmail.com>
2024-03-28 15:39:08 +0100

    results of gliner added

--- scripts_experiment/eval_ner.py
+++ scripts_experiment/eval_ner.py
@@ -17,6 +17,11 @@ def eval_ner(data, model_result):
             start_pos2 = int(entity2["start_pos"])
             end_pos2 = int(entity2["end_pos"])
             ent_type2 = entity2["type"]
+            if ent_type2=="persona":
+                ent_type2="PER"
+            elif ent_type2=="luogo":
+                ent_type2="LOC"
+
             char_intersection = len(set(range(start_pos1, end_pos1)).intersection(set(range(start_pos2, end_pos2))))
             if id2 == id1 and char_intersection > 0 and ent_type1==ent_type2:
                 matches.append(entity1)
@@ -35,7 +40,7 @@ def eval_ner(data, model_result):
     recall = len(matches) / (len(matches) + len(fn))
     f1 = (2 * precision * recall) / (precision + recall)
 
-    with open("../results/wikiann_final/results.txt", "w") as output:
+    with open("../results/gliner_final/results.txt", "w") as output:
         output.write("True Positives: " + str(len(tp)) + "\n\n")
         output.write("False Positives: " + str(len(fp)) + "\n\n")
         output.write("False Negatives: " + str(len(fn)) + "\n\n")
@@ -47,31 +52,31 @@ def eval_ner(data, model_result):
     n_keys = fn[0].keys()
     fp_keys = fp[0].keys()
 
-    tp_file = open("../results/wikiann_final/tp_ner.csv", "w", encoding="utf-8")
+    tp_file = open("../results/gliner_final/tp_ner.csv", "w", encoding="utf-8")
     dict_writer = csv.DictWriter(tp_file, p_keys)
     dict_writer.writeheader()
     dict_writer.writerows(matches)
     tp_file.close()
 
-    fp_file = open("../results/wikiann_final/fp_ner.csv", "w", encoding="utf-8")
+    fp_file = open("../results/gliner_final/fp_ner.csv", "w", encoding="utf-8")
     dict_writer = csv.DictWriter(fp_file, fp_keys)
     dict_writer.writeheader()
     dict_writer.writerows(fp)
     fp_file.close()
 
-    fn_file = open("../results/wikiann_final/fn_ner.csv", "w", encoding="utf-8")
+    fn_file = open("../results/gliner_final/fn_ner.csv", "w", encoding="utf-8")
     dict_writer = csv.DictWriter(fn_file, n_keys)
     dict_writer.writeheader()
     dict_writer.writerows(fn)
     fn_file.close()
 
 
-with open("../scripts_extraction/annotations_no_work_final.csv", "r", encoding="utf-8") as f2:
+with open("../scripts_extraction/annotations_no_work.csv", "r", encoding="utf-8") as f2:
     data = csv.DictReader(f2)
     data = list(data)
 f2.close()
 
-with open("../results/wikiann_final/output.csv", "r", encoding="utf-8") as f3:
+with open("../results/gliner_final/output.csv", "r", encoding="utf-8") as f3:
     model_result = csv.DictReader(f3)
     model_result = list(model_result)
 f3.close()
---
+++ scripts_experiment/run_gliner.py
@@ -0,0 +1,36 @@
+from gliner import GLiNER
+import csv
+from tqdm import tqdm
+
+model = GLiNER.from_pretrained("DeepMount00/universal_ner_ita", token="put token for gated model")
+
+labels = ["persona", "luogo"]
+
+with open("../scripts_extraction/paragraphs.csv", "r", encoding="utf-8") as f:
+    data = csv.DictReader(f)
+    data = list(data)
+f.close()
+
+pbar = tqdm(total=len(data))
+entita_nominate = []
+for row in data:
+    doc_id = row["id"]
+    doc_txt = row["text"]
+    entities = model.predict_entities(doc_txt, labels)
+    for entity in entities:
+        entry = {"id":doc_id,
+                 "surface_form":entity["text"],
+                 "start_pos":entity["start"],
+                 "end_pos":entity["end"],
+                 "type":entity["label"]}
+        entita_nominate.append(entry)
+    pbar.update(1)
+pbar.close()
+
+keys = entita_nominate[0].keys()
+with open("../results/gliner_final/output.csv", "w", encoding="utf-8") as f:
+    dict_writer = csv.DictWriter(f, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(entita_nominate)
+
+f.close()
