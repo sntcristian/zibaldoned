ab99e6a4aa2b2fb0c78c65fb55eaac30acc4839a
sntcristian <snt.cristian@gmail.com>
2024-05-06 12:30:13 +0200

    scripts extraction modified

--- scripts_extraction/scrape_paragraphs.py
+++ scripts_extraction/scrape_paragraphs.py
@@ -5,7 +5,7 @@ import re
 
 
 
-initial_paragraph = "https://digitalzibaldone.net/node/p2700_1"
+initial_paragraph = "https://digitalzibaldone.net/node/p1_1"
 
 output = []
 places = []
@@ -20,8 +20,8 @@ def scrape_paragraphs_recursive(paragraph_num):
             nextprev_div = soup.find('div', class_='nextprev')
             second_link = nextprev_div.find_all('a')[1]  # Secondo link
             second_link_href = second_link.get("href")
-            page_num = re.match("https:\/\/digitalzibaldone\.net\/node\/p(.*?)\_.*?", second_link_href).group(1)
-            if int(page_num)>3000:
+            page_num = re.match("https:\/\/digitalzibaldone\.net\/node\/p(.*?_.*?)", second_link_href).group(1)
+            if page_num=="4525_7":
                 next_page = None
             else:
                 next_page = second_link_href
@@ -50,25 +50,25 @@ def scrape_paragraphs_recursive(paragraph_num):
 scrape_paragraphs_recursive(initial_paragraph)
 
 o_keys = output[0].keys()
-with open("../data/paragraphs.csv", "w", encoding="utf-8") as f:
+with open("../data/paragraphs_all.csv", "w", encoding="utf-8") as f:
     dict_writer = csv.DictWriter(f, o_keys)
     dict_writer.writeheader()
     dict_writer.writerows(output)
 
 p_keys = places[0].keys()
-with open("places.csv", "w", encoding="utf-8") as f:
+with open("places_all.csv", "w", encoding="utf-8") as f:
     dict_writer = csv.DictWriter(f, p_keys)
     dict_writer.writeheader()
     dict_writer.writerows(places)
 
 p_keys = persons[0].keys()
-with open("people.csv", "w", encoding="utf-8") as f:
+with open("people_all.csv", "w", encoding="utf-8") as f:
     dict_writer = csv.DictWriter(f, p_keys)
     dict_writer.writeheader()
     dict_writer.writerows(persons)
 
 w_keys = persons[0].keys()
-with open("works.csv", "w", encoding="utf-8") as f:
+with open("works_all.csv", "w", encoding="utf-8") as f:
     dict_writer = csv.DictWriter(f, w_keys)
     dict_writer.writeheader()
     dict_writer.writerows(works)
