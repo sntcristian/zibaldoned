216e0edaae839d3e5770d4c0fe1e12d7e84eeb7b
sntcristian <snt.cristian@gmail.com>
2024-05-06 16:04:47 +0200

    algorithm for scraping updated (final)

--- scripts_extraction/scrape_paragraphs.py
+++ scripts_extraction/scrape_paragraphs.py
@@ -5,7 +5,7 @@ import re
 
 
 
-initial_paragraph = "https://digitalzibaldone.net/node/p1_1"
+initial_paragraph = "https://digitalzibaldone.net/node/p4525_6"
 
 output = []
 places = []
@@ -18,13 +18,16 @@ def scrape_paragraphs_recursive(paragraph_num):
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             nextprev_div = soup.find('div', class_='nextprev')
-            second_link = nextprev_div.find_all('a')[1]  # Secondo link
-            second_link_href = second_link.get("href")
-            page_num = re.match("https:\/\/digitalzibaldone\.net\/node\/p(.*?_.*?)", second_link_href).group(1)
-            if page_num=="4525_7":
-                next_page = None
+            if paragraph_num == "https://digitalzibaldone.net/node/p1_1":
+                link_p_seguente = nextprev_div.find_all('a')[0]
+            elif paragraph_num == "https://digitalzibaldone.net/node/p4525_7":
+                link_p_seguente = None
             else:
-                next_page = second_link_href
+                link_p_seguente = nextprev_div.find_all('a')[1]
+            if link_p_seguente != None:
+                next_page = link_p_seguente.get("href")
+            else:
+                next_page = None
             print(next_page)
             node = soup.find('div', id=paragraph_num.replace("https://digitalzibaldone.net/node/", ""))
             if node:
@@ -49,26 +52,30 @@ def scrape_paragraphs_recursive(paragraph_num):
 
 scrape_paragraphs_recursive(initial_paragraph)
 
-o_keys = output[0].keys()
-with open("../data/paragraphs_all.csv", "w", encoding="utf-8") as f:
-    dict_writer = csv.DictWriter(f, o_keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(output)
+if len(output)>0:
+    o_keys = output[0].keys()
+    with open("../data/paragraphs_all.csv", "w", encoding="utf-8") as f:
+        dict_writer = csv.DictWriter(f, o_keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(output)
 
-p_keys = places[0].keys()
-with open("places_all.csv", "w", encoding="utf-8") as f:
-    dict_writer = csv.DictWriter(f, p_keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(places)
+if len(places)>0:
+    p_keys = places[0].keys()
+    with open("places_all.csv", "w", encoding="utf-8") as f:
+        dict_writer = csv.DictWriter(f, p_keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(places)
 
-p_keys = persons[0].keys()
-with open("people_all.csv", "w", encoding="utf-8") as f:
-    dict_writer = csv.DictWriter(f, p_keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(persons)
+if len(persons)>0:
+    p_keys = persons[0].keys()
+    with open("people_all.csv", "w", encoding="utf-8") as f:
+        dict_writer = csv.DictWriter(f, p_keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(persons)
 
-w_keys = persons[0].keys()
-with open("works_all.csv", "w", encoding="utf-8") as f:
-    dict_writer = csv.DictWriter(f, w_keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(works)
+if len(works)>0:
+    w_keys = works[0].keys()
+    with open("works_all.csv", "w", encoding="utf-8") as f:
+        dict_writer = csv.DictWriter(f, w_keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(works)
