a9d7ab3f054da575f1d1d4485ce7d48428f11d93
sntcristian <snt.cristian@gmail.com>
2024-07-12 10:17:46 +0200

    readme update

--- scripts_NER/run_gliner.py
+++ scripts_NER/run_gliner.py
@@ -2,15 +2,16 @@ from gliner import GLiNER
 import csv
 from tqdm import tqdm
 
-model = GLiNER.from_pretrained("DeepMount00/universal_ner_ita", token="put token for gated model")
+model = GLiNER.from_pretrained("DeepMount00/GLiNER_ITA_LARGE")
 
-labels = ["persona", "luogo"]
+labels = ["persona", "opera"]
 
 with open("../data/paragraphs.csv", "r", encoding="utf-8") as f:
     data = csv.DictReader(f)
     data = list(data)
 f.close()
 
+data = data[:4]
 pbar = tqdm(total=len(data))
 entita_nominate = []
 for row in data:
@@ -28,9 +29,23 @@ for row in data:
 pbar.close()
 
 keys = entita_nominate[0].keys()
-with open("../results/gliner_final/output.csv", "w", encoding="utf-8") as f:
+with open("../results/gliner_final/output_test.csv", "w", encoding="utf-8") as f:
     dict_writer = csv.DictWriter(f, keys)
     dict_writer.writeheader()
     dict_writer.writerows(entita_nominate)
 
 f.close()
+
+
+# from gliner import GLiNER
+#
+# model = GLiNER.from_pretrained("DeepMount00/GLiNER_ITA_LARGE")
+#
+# text = """..."""
+#
+# labels = ["label1", "label2"]
+#
+# entities = model.predict_entities(text, labels)
+#
+# for entity in entities:
+#     print(entity["text"], "=>", entity["label"])
